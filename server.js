d72e3b5a3f08008c832777f285be107a74da7d2c
asharma1398 <asharma1398@gmail.com>
2020-07-08 18:02:54 -0400

    post working upon page refresh

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 var express = require("express");
 var fs = require("fs");
 
+// needed variable(s)
+// ==============================================================
+var note = [];
+
 // express app set up
 // ===============================================================
 var app = express();
@@ -24,19 +28,52 @@ app.get("*", (req, res) => {
 
 // notes page 
 app.get("/notes", (req, res) => {
-    res.sendFile(__dirname, "/public/add.html");
+    res.sendFile(__dirname + "/public/notes.html");
 });
 
 // get notes api
 app.get("/api/notes", (req, res) => {
-    res.sendFile(__dirname+"/db/db.json");
+    res.sendFile(__dirname + "/db/db.json");
+
+    
 });
 
 // post notes api
+app.post("/api/notes", (req, res) => {
+    
+    fs.readFile(__dirname + "/db/db.json", "utf8", (error, data) => {
+
+        if (error) {
+          return console.log(error);
+        }
+        else {
+            
+            note = JSON.parse(data);
+            console.log(note);
+        }      
+
+        req.body.id = note[note.length - 1].id + 1;
+
+        note.push(req.body);
+
+        console.log("note", note);
+
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(note), (error, data) => {
+            if (error) {
+                return console.log(error);
+            } else {
+                console.log("Success!")
+            }
+    
+        })
+
+      });
+    
+});
 
 // server starts listening
 // ===============================================================
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
-  });
+});
   
