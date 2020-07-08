63737ac58739e3900be9b87b278aac7badeba236
asharma1398 <asharma1398@gmail.com>
2020-07-08 08:36:16 -0400

    data parsing, routes, and more set up added

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ var express = require("express");
 // express app set up
 // ============================================================
 var app = express();
-var PORT = 8000;
+var PORT = process.env.PORT || 8000;
+
+// handles data parsing
+// ============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+// =============================================================
+app.use(express.static("public"));
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+app.get("/notes", function(req, res) {
+res.sendFile(path.join(__dirname, "/public/add.html"));
+});
 
 // server starts listening
 // ============================================================
