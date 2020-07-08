9ca967722eee7361a6bf0aefaa3a84b6c4b7fa42
asharma1398 <asharma1398@gmail.com>
2020-07-08 09:07:36 -0400

    get api outlined

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
 // dependency
-// ============================================================
+// ===============================================================
 var express = require("express");
+var fs = require("fs");
 
 // express app set up
-// ============================================================
+// ===============================================================
 var app = express();
 var PORT = process.env.PORT || 8000;
 
 // handles data parsing
-// ============================================================
+// ===============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // routes
-// =============================================================
+// ===============================================================
 app.use(express.static("public"));
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+// main page
+app.get("*", (req, res) => {
+    res.sendFile(__dirname, "/public/index.html");
   });
 
-app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "/public/add.html"));
+// notes page 
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname, "/public/add.html");
 });
 
+// get notes api
+app.get("/api/notes", (req, res) => {
+    res.sendFile(__dirname+"/db/db.json");
+});
+
+// post notes api
+
 // server starts listening
-// ============================================================
+// ===============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
